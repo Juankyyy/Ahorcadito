8b534bfea727030c3ab21e9a8da6a6f4e234807a
Juanky <juancamiloherrera960@gmail.com>
2023-12-03 21:09:43 -0500

    Seleccionar tema

--- script.js
+++ script.js
@@ -1,55 +1,90 @@
-// const palabras = ["error", "iterador", "riwi", "javascript"]
-const palabras = ["amarillo", "azul", "rojo", "verde", "morado"]
-const palabra = palabras[Math.floor(Math.random() * palabras.length)];
+const temas = [["error", "booleano", "riwi", "funcion", "string"], ["amarillo", "azul", "rojo", "verde", "morado", "blanco"], ["pizza", "hamburguesa", "lasaña", "salchipapas", "empanada", "buñuelo"]]
+let tema = 0;
 
-const palabraArray = palabra.split("");
-let vidas = 6;
-let correcto = false;
-let arraySecret = "_".repeat(palabra.length).split("");
+const select = document.querySelector(".select")
+const content = document. querySelector("#content")
 
-const wordSecret = document.querySelector("#arraySecret");
-const lives = document.querySelector("#lives");
-const inputLetter = document.querySelector("#letter-input");
-const btn = document.querySelector(".letter-btn");
+const programming = document.querySelector("#programming");
+const colors = document.querySelector("#colors");
+const food = document.querySelector("#food");
 
-lives.textContent = `Tienes ${vidas} Vidas`;
-wordSecret.textContent = arraySecret.join(" ");
-
-btn.addEventListener("click", () => {
-    const letra = inputLetter.value.toLowerCase();
-    inputLetter.value = "";
-
-    intento(letra);
+programming.addEventListener("click", () => {
+    tema = temas[0];
+    hidden();
+    game();
 });
 
+colors.addEventListener("click", () => {
+    tema = temas[1];
+    hidden();
+    game();
+});
 
-function intento(letra) {
-    correcto = false;
+food.addEventListener("click", () => {
+    tema = temas[2];
+    hidden();
+    game();
+});
 
-    for (j = 0; j < palabra.length; j++) {
-    
-        if (palabra[j] == letra) {
-            arraySecret[j] = letra;
-            correcto = true;
-    
-            wordSecret.textContent = arraySecret.join(" ").toUpperCase();
-        };
-    };
-
-    if (!correcto) {
-        vidas--;
-        lives.textContent = `Tienes ${vidas} Vidas`;
-        alert("Incorrecto")
-    };
-
-    if (palabraArray.join("") == arraySecret.join("")) {
-        alert("Ganaste")
-    };
-    
-    if (vidas == 0) {
-        alert("Perdiste bobo")
-    };
+function hidden() {
+    select.classList.add("hidden")
+    content.classList.remove("hidden")
 }
+
+function game() {
+    const palabra = tema[Math.floor(Math.random() * tema.length)];
+    
+    const palabraArray = palabra.split("");
+    vidas = 6;
+    correcto = false;
+    arraySecret = "_".repeat(palabra.length).split("");
+
+    const wordSecret = document.querySelector("#arraySecret");
+    const lives = document.querySelector("#lives");
+    const inputLetter = document.querySelector("#letter-input");
+    const btn = document.querySelector(".letter-btn");
+    
+    lives.textContent = `Tienes ${vidas} Vidas`;
+    wordSecret.textContent = arraySecret.join(" ");
+    
+    btn.addEventListener("click", () => {
+        const letra = inputLetter.value.toLowerCase();
+        inputLetter.value = "";
+    
+        intento(letra);
+    });
+    
+    
+    function intento(letra) {
+        correcto = false;
+    
+        for (j = 0; j < palabra.length; j++) {
+        
+            if (palabra[j] == letra) {
+                arraySecret[j] = letra;
+                correcto = true;
+        
+                wordSecret.textContent = arraySecret.join(" ").toUpperCase();
+            };
+        };
+    
+        if (!correcto) {
+            vidas--;
+            lives.textContent = `Tienes ${vidas} Vidas`;
+            alert("Incorrecto")
+        };
+    
+        if (palabraArray.join("") == arraySecret.join("")) {
+            alert("Ganaste")
+        };
+        
+        if (vidas == 0) {
+            alert("Perdiste bobo")
+        };
+    }
+}
+
+
 
 // while (vidas != 0) {
 //     let correcto = false;
